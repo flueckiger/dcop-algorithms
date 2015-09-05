354d2561dbd3908ef12754296361e1cb892c14c4
Andreas Flueckiger <andreas@flueckiger.ch>
2015-09-05 22:39:25 +0200

    Added utilityBounds to trait UtilityFunction.

--- src/main/scala/com/signalcollect/dcop/impl/UtilityFunctions.scala
+++ src/main/scala/com/signalcollect/dcop/impl/UtilityFunctions.scala
@@ -5,9 +5,19 @@ import com.signalcollect.dcop.modules._
 trait VertexColoringUtility[AgentId, Action, Config <: Configuration[AgentId, Action, Config], UtilityType] extends UtilityFunction[AgentId, Action, Config, UtilityType] {
   override def computeUtilities(c: Config, a: Action) =
     c.neighborhood.transform((_, x) => if (x == a) utilEv.zero else utilEv.one)
+
+  override def utilityBounds(c: Config) = {
+    val x = (utilEv.zero, utilEv.one)
+    c.neighborhood.mapValues(_ => x)
+  }
 }
 
 trait ConflictBasedVertexColoringUtility[AgentId, Action, Config <: Configuration[AgentId, Action, Config], UtilityType] extends UtilityFunction[AgentId, Action, Config, UtilityType] {
   override def computeUtilities(c: Config, a: Action) =
     c.neighborhood.transform((_, x) => if (x == a) utilEv.fromInt(-1) else utilEv.zero)
+
+  override def utilityBounds(c: Config) = {
+    val x = (utilEv.fromInt(-1), utilEv.zero)
+    c.neighborhood.mapValues(_ => x)
+  }
 }
--- src/main/scala/com/signalcollect/dcop/modules/UtilityFunction.scala
+++ src/main/scala/com/signalcollect/dcop/modules/UtilityFunction.scala
@@ -11,4 +11,11 @@ trait UtilityFunction[AgentId, Action, Config <: Configuration[AgentId, Action, 
   def computeUtility(c: Config, a: Action): UtilityType = computeUtilities(c, a).values.sum
 
   def computeUtility(c: Config): UtilityType = computeUtilities(c).values.sum
+
+  def utilityBounds(c: Config): Map[AgentId, (UtilityType, UtilityType)]
+
+  def utilityBound(c: Config): (UtilityType, UtilityType) = {
+    val x = utilityBounds(c).values
+    (x.map(_._1).min, x.map(_._2).max)
+  }
 }
